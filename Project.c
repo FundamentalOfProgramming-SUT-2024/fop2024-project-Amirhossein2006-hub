d23e8827a366da3b200fb4af452a5635d221ec5b
Amirhossein2006-hub <a.h.ayoubian@gmial.com>
2025-01-03 10:39:47 +0330

    Sign_in Created

--- Project.c
+++ Project.c
@@ -1,11 +1,26 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int menu();
 void sign_in();
+int ivalue(char username[], char password[], char email[]);
+int username_check(char username[]);
+int password_check(char password[]);
+int email_check(char email[]);
 void login();
 void settings();
 void profile();
 void score_table();
+
+typedef struct
+{
+    char username[100];
+    char password[100];
+    char email[100];
+} Player;
+
 
 int main()
 {
@@ -13,6 +28,7 @@ int main()
     noecho();
     curs_set(FALSE);
     keypad(stdscr, TRUE);
+    start_color();
 
     switch (menu())
     {
@@ -20,16 +36,16 @@ int main()
         sign_in();
         break;
     case 1:
-        login();
+        //login();
         break;
     case 2:
-        settings();
+        //settings();
         break;
     case 3:
-        profile();
+        //profile();
         break;
     case 4:
-        score_table();
+        //score_table();
         break;
     }
 
@@ -77,6 +93,165 @@ int menu()
         }
     }
 
-    clear();
     return choice;
 }
+
+void sign_in()
+{
+    clear();
+    Player player;
+    char username[100];
+    char password[100];
+    char email[100];
+
+    echo();
+    curs_set(TRUE);
+
+    mvprintw(11, 50, "Username: ");
+    mvprintw(12, 50, "Password: ");
+    mvprintw(13, 50, "Email: ");
+    mvgetstr(11, 60, username);
+    mvgetstr(12, 60, password);
+    mvgetstr(13, 57, email);
+    
+    noecho();
+    curs_set(FALSE);
+
+    if (ivalue(username, password, email) == 0) sign_in();
+    else
+    {
+        FILE *players_info = fopen("Players_Info.txt", "a");
+        fprintf(players_info, "username: (%s), password: (%s), email: (%s)\n", username, password, email);
+    }
+    clear();
+}
+
+int ivalue(char username[], char password[], char email[])
+{
+    if (username_check(username) && password_check(password) && email_check(email)) return 1;
+
+    return 0;
+}
+
+int username_check(char username[])
+{
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    FILE *players_info = fopen("Players_Info.txt", "r");
+
+    char temp[1000];
+    while (fgets(temp, sizeof(temp), players_info))
+    {
+        char user[100];
+        int j;
+        for (int i = 11; temp[i] != ')'; i++)
+            user[j++] = temp[i];
+        
+        user[j] = '\0';
+        if (strcmp(user, username) == 0)
+        {
+            clear();
+            attron(COLOR_PAIR(1));
+            mvprintw(15, 45, "Username is already used");
+            attroff(COLOR_PAIR(1));
+            mvprintw(16, 46, "Press any key to reset");
+            getch();
+            fclose(players_info);
+            return 0;
+        }
+    }
+    fclose(players_info);
+    return 1;
+}
+
+int password_check(char password[])
+{
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    if (strlen(password) < 7)
+    {
+        clear();
+        attron(COLOR_PAIR(1));
+        mvprintw(15, 40, "Password length must be at least 7");
+        attroff(COLOR_PAIR(1));
+        mvprintw(16, 47, "Press any key to reset");
+        getch();
+        return 0;
+    }
+
+    int is_digit = 1, is_upper = 1, is_lower = 1;
+    for (int i = 0; i < strlen(password); i++)
+    {
+        if (isdigit(password[i])) is_digit = 0;
+        if (isupper(password[i])) is_upper = 0;
+        if (islower(password[i])) is_lower = 0;
+    }
+    
+    if (is_digit || is_upper || is_lower)
+    {
+        clear();
+        attron(COLOR_PAIR(1));
+        mvprintw(15, 15, "Password must include at least 1 digit, 1 uppercase charecter and 1 lowercase charecter");
+        attroff(COLOR_PAIR(1));
+        mvprintw(16, 45, "Press any key to reset");
+        getch();
+        return 0;
+    }
+
+    return 1;
+}
+
+int email_check(char email[])
+{
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    int atCount = 0;
+    int dotCount = 0;
+    int atPosition = -1;
+    int dotPosition = -1;
+
+    for (int i = 0; i < strlen(email); i++) {
+        char c = email[i];
+        if (c == '@') 
+        {
+            atCount++;
+            atPosition = i;
+        }
+        else if (c == '.') 
+        {
+            dotCount++;
+            dotPosition = i;
+        }
+        else if (!isalnum(c) && c != '_' && c != '-') 
+        {
+            clear();
+            attron(COLOR_PAIR(1));
+            mvprintw(15, 50, "Email invalid");
+            attroff(COLOR_PAIR(1));
+            mvprintw(16, 45, "Press any key to reset");
+            getch();
+            return 0;
+        }
+    }
+
+    if (atCount != 1)
+    {
+        clear();
+        attron(COLOR_PAIR(1));
+        mvprintw(15, 50, "Email invalid");
+        attroff(COLOR_PAIR(1));
+        mvprintw(16, 45, "Press any key to reset");
+        getch();
+        return 0;
+    }
+    if (atPosition > dotPosition || dotPosition == -1 || dotPosition == strlen(email) - 1)
+    {
+        clear();
+        attron(COLOR_PAIR(1));
+        mvprintw(15, 50, "Email invalid");
+        attroff(COLOR_PAIR(1));
+        mvprintw(16, 45, "Press any key to reset");
+        getch();
+        return 0;
+    }
+
+    return 1;
+}
+
